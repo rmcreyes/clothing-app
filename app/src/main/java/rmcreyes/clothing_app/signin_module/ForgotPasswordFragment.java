8dd8b2dfd5f2a0765a9af204e0be2f56bf281ba8
Roberto <rmcreyes.eng@gmail.com>
2018-05-14 19:38:18 -0700

    add stringchecker

--- app/src/main/java/rmcreyes/clothing_app/signin_module/ForgotPasswordFragment.java
+++ app/src/main/java/rmcreyes/clothing_app/signin_module/ForgotPasswordFragment.java
@@ -2,10 +2,15 @@ package rmcreyes.clothing_app.signin_module;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import rmcreyes.clothing_app.R;
 
@@ -13,6 +18,11 @@ import rmcreyes.clothing_app.R;
  * A simple {@link Fragment} subclass.
  */
 public class ForgotPasswordFragment extends Fragment {
+
+    private EditText email_edit;
+    private TextView email_error;
+    private Button reset_btn;
+
 
 
     public ForgotPasswordFragment() {
@@ -27,4 +37,15 @@ public class ForgotPasswordFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_forgot_password, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        email_edit = view.findViewById(R.id.email_edit);
+        email_error = view.findViewById(R.id.email_error);
+        reset_btn = view.findViewById(R.id.reset_btn);
+
+        // initially set the error message invisible
+        email_error.setVisibility(View.INVISIBLE);
+    }
 }
---
+++ app/src/main/java/rmcreyes/clothing_app/signin_module/StringChecker.java
@@ -0,0 +1,10 @@
+package rmcreyes.clothing_app.signin_module;
+
+/**
+ * Created by robin on 2018-05-14.
+ */
+
+public class StringChecker {
+
+    public static
+}
