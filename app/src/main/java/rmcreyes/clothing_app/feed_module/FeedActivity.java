f4fcc87eac4543a028f50164c384e08c676b1d5f
Roberto <rmcreyes.eng@gmail.com>
2018-05-14 22:14:49 -0700

    add FeedActivity

---
+++ app/src/main/java/rmcreyes/clothing_app/feed_module/FeedActivity.java
@@ -0,0 +1,15 @@
+package rmcreyes.clothing_app.feed_module;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import rmcreyes.clothing_app.R;
+
+public class FeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+    }
+}
