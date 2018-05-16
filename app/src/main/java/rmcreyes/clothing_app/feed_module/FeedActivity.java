5527ee7ca769628c755bed6b9335a99b95a200b5
Roberto <rmcreyes.eng@gmail.com>
2018-05-16 01:52:20 -0700

    added tab switching

--- app/src/main/java/rmcreyes/clothing_app/feed_module/FeedActivity.java
+++ app/src/main/java/rmcreyes/clothing_app/feed_module/FeedActivity.java
@@ -1,15 +1,54 @@
 package rmcreyes.clothing_app.feed_module;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import rmcreyes.clothing_app.R;
 
 public class FeedActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawerLayout;
+    private FeedCollectionPagerAdapter pagerAdapter;
+    private ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+
+        setTitle("");
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        // set up drawer button
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_nav_btn_temp);
+
+        // set up pager adapter
+        pagerAdapter = new FeedCollectionPagerAdapter(getSupportFragmentManager());
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        viewPager.setAdapter(pagerAdapter);
+
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/rmcreyes/clothing_app/feed_module/FeedCollectionPagerAdapter.java
@@ -0,0 +1,46 @@
+package rmcreyes.clothing_app.feed_module;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import rmcreyes.clothing_app.signin_module.ForgotPasswordFragment;
+import rmcreyes.clothing_app.signin_module.TermsFragment;
+
+/**
+ * Created by robin on 2018-05-16.
+ * taken from the android developers tutorial at
+ * https://developer.android.com/training/implementing-navigation/lateral
+ */
+
+public class FeedCollectionPagerAdapter extends FragmentStatePagerAdapter {
+
+    private List<Fragment> FragmentList;
+
+    public FeedCollectionPagerAdapter(FragmentManager fm) {
+        super(fm);
+        FragmentList = new ArrayList<Fragment>();
+        FragmentList.add(new TermsFragment());
+        FragmentList.add(new ForgotPasswordFragment());
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if(position == 0) {
+            return new TermsFragment();
+        }
+        else
+            return new ForgotPasswordFragment();
+    }
+
+    @Override
+    public int getCount() {
+        return FragmentList.size();
+    }
+
+}
