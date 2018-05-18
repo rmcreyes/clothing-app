829fcecbcc82a8a7d14e61bc759043b041257aab
Roberto <rmcreyes.eng@gmail.com>
2018-05-17 18:44:05 -0700

    implemented tabs with swipes

--- app/src/main/java/rmcreyes/clothing_app/feed_module/FeedActivity.java
+++ app/src/main/java/rmcreyes/clothing_app/feed_module/FeedActivity.java
@@ -1,5 +1,7 @@
 package rmcreyes.clothing_app.feed_module;
 
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
@@ -8,14 +10,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import rmcreyes.clothing_app.R;
 
 public class FeedActivity extends AppCompatActivity {
 
-    private DrawerLayout mDrawerLayout;
+    private DrawerLayout drawerLayout;
+    private Toolbar search_bar;
     private FeedCollectionPagerAdapter pagerAdapter;
     private ViewPager viewPager;
+    private ImageButton trending_btn, following_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,19 +31,64 @@ public class FeedActivity extends AppCompatActivity {
 
         setTitle("");
 
-        mDrawerLayout = findViewById(R.id.drawer_layout);
+        drawerLayout= findViewById(R.id.drawer_layout);
+        search_bar = findViewById(R.id.search_bar);
+        trending_btn = findViewById(R.id.trending_btn);
+        following_btn = findViewById(R.id.following_btn);
 
         // set up drawer button
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setSupportActionBar(search_bar);
         ActionBar actionbar = getSupportActionBar();
         actionbar.setDisplayHomeAsUpEnabled(true);
-        actionbar.setHomeAsUpIndicator(R.drawable.ic_nav_btn_temp);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_nav_drawer_btn);
+
 
         // set up pager adapter
         pagerAdapter = new FeedCollectionPagerAdapter(getSupportFragmentManager());
         viewPager = (ViewPager) findViewById(R.id.pager);
         viewPager.setAdapter(pagerAdapter);
+
+        trending_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                trending_btn.setBackgroundResource(R.drawable.ic_fire_prime);
+                following_btn.setBackgroundResource(R.drawable.ic_follow_white);
+                viewPager.setCurrentItem(0);
+            }
+        });
+
+        following_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                trending_btn.setBackgroundResource(R.drawable.ic_fire_white);
+                following_btn.setBackgroundResource(R.drawable.ic_follow_prime);
+                viewPager.setCurrentItem(1);
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if(position == 0) {
+                    trending_btn.setBackgroundResource(R.drawable.ic_fire_prime);
+                    following_btn.setBackgroundResource(R.drawable.ic_follow_white);
+                }
+                else if(position == 1) {
+                    trending_btn.setBackgroundResource(R.drawable.ic_fire_white);
+                    following_btn.setBackgroundResource(R.drawable.ic_follow_prime);
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
 
 
 
@@ -46,7 +98,7 @@ public class FeedActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
-                mDrawerLayout.openDrawer(GravityCompat.START);
+                drawerLayout.openDrawer(GravityCompat.START);
                 return true;
         }
         return super.onOptionsItemSelected(item);
