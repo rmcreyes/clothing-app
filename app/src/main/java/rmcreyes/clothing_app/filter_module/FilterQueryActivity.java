50d8f69999bec68ca0c82970cfe2b722c1c00c98
Roberto <rmcreyes.eng@gmail.com>
2018-06-17 12:37:00 -0700

    fix color sheet

---
+++ app/src/main/java/rmcreyes/clothing_app/filter_module/FilterQueryActivity.java
@@ -0,0 +1,15 @@
+package rmcreyes.clothing_app.filter_module;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import rmcreyes.clothing_app.R;
+
+public class FilterQueryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter_query);
+    }
+}
