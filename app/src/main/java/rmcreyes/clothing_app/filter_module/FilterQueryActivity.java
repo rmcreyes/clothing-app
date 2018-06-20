1a7ce78eaebad2c05881eed17ef0bc3f189afc47
Roberto <rmcreyes.eng@gmail.com>
2018-06-19 19:47:02 -0700

    change actiobar color

--- app/src/main/java/rmcreyes/clothing_app/filter_module/FilterQueryActivity.java
+++ app/src/main/java/rmcreyes/clothing_app/filter_module/FilterQueryActivity.java
@@ -1,5 +1,8 @@
 package rmcreyes.clothing_app.filter_module;
 
+import android.app.ActionBar;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +14,7 @@ public class FilterQueryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter_query);
+
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.colorPrimary)));
     }
 }
